748831a056dc71fa54992a9e9cf8e74091116ce5
George Hill <blocker1.gh@gmail.com>
2021-09-18 14:24:04 +0100

    implemented fastf1

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import json
-import data
-
+import ergast
+import fasterf1
 
 app = Flask(__name__)
 
@@ -24,7 +24,7 @@ else:
 
 @app.route('/')
 def notdash():
-    df = data.RequestParams(2020, 7, [1], ['Norris', 'Hamilton'], limit='100').laptimes()
+    df = ergast.RequestParams(2020, 7, [1], ['Norris', 'Hamilton'], limit='100').laptimes()
     #df = data.standings(type = 'driverStandings', season = 'all')
     fig = make_subplots() #specs=[[{"secondary_y": True}]]
     fig.add_trace(go.Scatter(y=df['Norris'].str[1], y0=df['Norris'].str[0], x=df['lap'], name='norris'))
@@ -36,10 +36,21 @@ def notdash():
     return render_template("notdash.html", graphJSON=graphJSON)
     #return fig.show()
 
+@app.route('/fasterf1')
+def fastestf1():
+    bot_data = fasterf1.get_fastest('BOT', 2021, 'monza', 'Q')
+    ham_data = fasterf1.get_fastest('HAM', 2021, 'monza', 'Q')
+
+    fig = make_subplots()
+    fig.add_trace(go.Scatter(y=bot_data['Speed'], x=bot_data['Time'], name='Bottas'))
+    fig.add_trace(go.Scatter(y=ham_data['Speed'], x=ham_data['Time'], name='Hamilton'))
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("notdash.html", graphJSON=graphJSON)
 
 @app.route('/12/9/2021')
 def old():
-    df = data.standings(type = 'driverStandings', season = 'all')
+    df = ergast.standings(type = 'driverStandings', season = 'all')
     fig = px.line(df, x="season", y="points", hover_data=['driverId'], markers=True)
     fig.add_bar(x=df['season'], y=df['round'])
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
---
+++ fasterf1.py
@@ -0,0 +1,38 @@
+import fastf1 as ff1
+from fastf1 import plotting
+from matplotlib import pyplot as plt
+
+plotting.setup_mpl()
+
+ff1.Cache.enable_cache('cache')  # optional but recommended
+
+def get_fastest(driver, year, track, session):
+    session = ff1.get_session(year, track, session)
+    laps = session.load_laps(with_telemetry=True)
+
+    fast_driver = laps.pick_driver(driver).pick_fastest()
+    driver_car_data = fast_driver.get_car_data()
+
+    return driver_car_data
+
+'''
+driver1 = 'RIC'
+driver2 = 'NOR'
+
+
+monza_quali = ff1.get_session(2021, 'Monza', 'Q')
+laps = monza_quali.load_laps(with_telemetry=True)
+
+fast_driver1 = laps.pick_driver(driver1).pick_fastest()
+fast_driver2 = laps.pick_driver(driver2).pick_fastest()
+driver1_car_data = fast_driver1.get_car_data()
+driver2_car_data = fast_driver2.get_car_data()
+
+fig, ax = plt.subplots()
+ax.plot(driver1_car_data['Time'], driver1_car_data['Speed'], color='red')
+ax.plot(driver2_car_data['Time'], driver2_car_data['Speed'], color='cyan')
+ax.set_title(f'{driver1} vs {driver2}')
+ax.set_xlabel('Time')
+ax.set_ylabel("Speed [km/h]")
+plt.show()
+'''
